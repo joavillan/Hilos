5fc379c697b9c088344161cea141a37c5b2b21b3
Joaquin Villegas <joa.villan@outlook.com>
2019-10-21 13:00:17 +0200

    Comit1

--- src/ej3/ContadorApplet.java
+++ src/ej3/ContadorApplet.java
@@ -4,63 +4,83 @@ import java.applet.Applet;
 import java.awt.*;
 import java.awt.event.*;
 
-public class ContadorApplet extends Applet implements Runnable, ActionListener{
+public class ContadorApplet extends Applet implements ActionListener{
 
-	private Thread h;
-	long CONTADOR = 0;
-	private boolean parar;
-	private Font fuente;
-	private Button b1,b2;//botones del Applet
-	
-	public void start() {};
-	
-	public void init() {
-		setBackground(Color.yellow);//color de fondo
-		add(b1=new Button("Iniciar contador"));
-		b1.addActionListener(this);
-		add(b2=new Button("Parar contador"));
-		b2.addActionListener(this);
-		fuente= new Font("Verdana", Font.BOLD, 26);//tipo letra
-	}
-
-	@Override
-	public void run(){
-		parar = false;
-		Thread hiloActual = Thread.currentThread();
-		while(h == hiloActual && !parar) {
-			try {
-				Thread.sleep(300);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
+	class HiloContador extends Thread{
+		
+		boolean paro;
+		long cont;
+		
+		HiloContador(long l){
+			this.cont = l;
+		}
+		
+		public void run() {
+			paro=false;
+			while (!paro) {
+				try {
+					Thread.sleep(300);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				repaint();
+				cont++;
 			}
-			repaint();
-			CONTADOR++;
+		}
+
+		public void setParo(boolean paro) {
+			this.paro = paro;
+		}
+
+		public long getCont() {
+			return cont;
 		}
 	}
 	
+	private HiloContador h1=new HiloContador(100);
+	private HiloContador h2=new HiloContador(28);
+	private Font fuente;
+	private Button b1,b2;
+	
+	public void start() {
+		h1.start();
+		h2.start();
+	};
+
+	public void init() {
+		this.setSize(400, 200);
+		setBackground(Color.yellow);
+		add(b1=new Button("Finalizar hilo 1"));
+		b1.addActionListener(this);
+		add(b2=new Button("Finalizar hilo 2"));
+		b2.addActionListener(this);
+		fuente=new Font("Verdana",Font.BOLD,26);
+	}
+
+
 	public void paint(Graphics g) {
-		g.clearRect(0, 0, 400, 400);
+		g.clearRect(0, 0,400,400);
 		g.setFont(fuente);
-		g.drawString(Long.toString((long)CONTADOR), 80, 100);
+		g.drawString("Hilo 1: "+Long.toString((long)h1.getCont()),80,100);
+		g.drawString("Hilo 2: "+Long.toString((long)h2.getCont()),80,150);
 	}
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if (e.getSource() == b1) {
-			b1.setLabel("Continuar");
-			if (h != null && h.isAlive()) {}
-			
-			else {
-				h = new Thread(this);
-				h.start();
-			}
-		}else if(e.getSource() == b2)
-			parar = true;
-		
+		// TODO Auto-generated method stub
+		if(e.getSource()==b1) {
+			b1.setLabel("Hilo 1 Finalizado");
+			h1.stop();
+			//h1.setParar();
+		}else {
+			b2.setLabel("Hilo 2 Finalizado");
+			//h2.setParar();
+			h2.stop();		}
 	}
 	
 	public void stop() {
-		h = null;
+		h1=null;
+		h2=null;
 	}
 
 }
